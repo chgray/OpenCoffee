302517682526b837b77aa140d499012626349c33
Chris Gray <chgray@gmail.com>
2023-01-16 15:51:21 -0800

    chagnes

---
+++ PID.py
@@ -0,0 +1,79 @@
+from machine import Pin, Timer
+import time
+import machine, onewire, ds18x20
+from lcd1602 import *
+
+from time import sleep
+
+global lcd
+# https://microcontrollerslab.com/ds18b20-raspberry-pi-pico-micropython-tutorial/
+
+print("Running.")
+
+led = Pin(25, Pin.OUT)
+button = Pin(12, Pin.IN, Pin.PULL_DOWN)
+timer = Timer()
+
+print("--------------------------------")
+count = 0
+temp=0.1
+heating=1
+
+
+def simulate_temp_change(timer):
+    #timet = 1
+    global temp
+    
+    if 0 != heating:
+        temp = (temp + 0.5)
+        led.value(1)
+    else:
+        temp = (temp - 0.25)
+        led.value(0)
+        
+    if temp < 0:
+        temp = 0
+    elif temp > 300:
+        temp = 300
+        
+    #print("NT: %f" % (temp))
+
+
+timer.init(freq=2.5, mode=Timer.PERIODIC, callback=simulate_temp_change)
+
+
+
+
+
+while 1:
+    b = button.value()
+    
+    try:
+        count = count + 1
+        print("Temp : %f" % (temp))
+        
+        if temp > 3:
+            heating = 0
+            
+        sleep(2)
+        if 0 == button.value():
+            print("button")
+            count += 100
+            sleep(1)
+            
+    except Exception as e:
+        print(e)
+        print("Not good..")
+        
+        
+    
+ 
+
+
+
+
+
+
+
+
+
