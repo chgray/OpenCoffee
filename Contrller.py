ad7c611469be0867d2f088c31faa0694fa975866
Chris Gray <chgray@gmail.com>
2023-01-16 15:31:52 -0800

    working

--- Contrller.py
+++ Contrller.py
@@ -37,12 +37,13 @@ roms = ds_sensor.scan()
  
 lcd.message('Found DS devices')
 lcd.message('Temperature (C)')
-
-
+sleep(2)
+lcd.clear()
 
 
 print("--------------------------------")
-while True:
+count = 0
+while count < 1000:
     b = button.value()
     
     try:
@@ -51,50 +52,29 @@ while True:
         for rom in roms:
             lcd.write(0,0, str(ds_sensor.read_temp(rom)))
         
+        lcd.write(0,1, str(count))
+        count = count + 1
         
-        #print("Button: %d" % (b))
         if 0 == button.value():
             led.toggle()
-            lcd.message("hello")
-            if 1 == state:
-                print("CLEAR")
-                lcd.clear()
-                
-            elif 2 == state:
-                lcd.send_command(LCD_DISPLAYCONTROL | LCD_DISPLAYON )
-                lcd.clear()
-                lcd.message("display_ctl + on")
-                
-            elif 3 == state:
-                lcd.send_command(LCD_DISPLAYCONTROL )
-               
-                
-            elif 4 == state:
-                lcd.send_command(LCD_DISPLAYCONTROL | LCD_DISPLAYON )
-          
-            state = state + 1
+            count += 100
             sleep(1)
-    except:
+            
+    except Exception as e:
+        print(e)
         print("Not good..")
         
         
     
-
-
-
+lcd.clear()
+lcd.message("BYE")
+print("BYE")
  
-while True:
-  ds_sensor.convert_temp()
-  #sleep(1)
-  for rom in roms:
-    print(ds_sensor.read_temp(rom))
-  #sleep(0.25)
- 
-
 def blink(timer):
     led.toggle()
 
 #timer.init(freq=2.5, mode=Timer.PERIODIC, callback=blink)
+
 
 
 
