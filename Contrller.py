57e268d593627199938968cea5ebb548c4c0ef1f
Chris Gray <chgray@gmail.com>
2023-01-15 22:34:27 -0800

    test

--- Contrller.py
+++ Contrller.py
@@ -1,13 +1,49 @@
 from machine import Pin, Timer
 import time
-import machine, onewire, ds18x20, boo
+import machine, onewire, ds18x20
+from lcd1602 import *
+
 from time import sleep
+
+
+global lcd
 # https://microcontrollerslab.com/ds18b20-raspberry-pi-pico-micropython-tutorial/
 
 
 led = Pin(25, Pin.OUT)
 button = Pin(12, Pin.IN, Pin.PULL_DOWN)
 timer = Timer()
+
+# https://docs.sunfounder.com/projects/thales-kit/en/latest/micropython/liquid_crystal_display.html#what-more
+#
+
+
+
+lcd = LCD()
+string = " Hello!\n"
+
+#lcd.clear()
+
+print("--------------------------------")
+while True:
+    b = button.value()
+    #print("Button: %d" % (b))
+    if 0 == button.value():
+        print("Button")
+        led.toggle()
+        #lcd.message("X");
+        lcd.write(1,1,"hi")
+        time.sleep(.25)
+        
+
+#lcd.message(string)
+#utime.sleep(2)
+#string = "    Sunfounder!"
+#lcd.message(string)
+#utime.sleep(2)
+#lcd.clear()
+
+
 
 print('Setting up Temp')
 ds_pin = machine.Pin(2) 
@@ -17,6 +53,10 @@ roms = ds_sensor.scan()
  
 print('Found DS devices')
 print('Temperature (°C)')
+
+
+
+
  
 while True:
   ds_sensor.convert_temp()
@@ -31,13 +71,8 @@ def blink(timer):
 
 #timer.init(freq=2.5, mode=Timer.PERIODIC, callback=blink)
 
-while True:
-    b = button.value()
-    #print("Button: %d" % (b))
-    if 0 == button.value():
-        print("Button")
-        led.toggle()
-        time.sleep(.25)
-        
+
+
+
 
 
