2511489e2981790785398676ccc7a1e83265969d
Chris Gray <chgray@gmail.com>
2023-01-21 20:35:30 -0800

    wifitest

--- WiFiTest.py
+++ WiFiTest.py
@@ -20,74 +20,88 @@ class WifiLog(object):
     m_password = 'deadbeef01'
     m_lock = _thread.allocate_lock()
     m_connected = False
+    m_Exit = False
+    
+    def close(self):
+        self.m_lock.acquire()
+        self.wlan.active(False)
+        m_connected = False
+        m_Exit = True
+        self.m_lock.release()
     
     def connect_thread(self):
-        while True:
-                print("Hi wifi")
-                print("Scanning:")
-                
-                scanlist = self.wlan.scan()
-                print("Got Scanlist")
-                for result in scanlist:
-                  ssid, bssid, channel, RSSI, authmode, hidden = result
-                  print("     %s == %s,  authmode=%d" % (ssid, ubinascii.hexlify(bssid), authmode))
+        try:
+            while True:
+                    print("Hi wifi")
+                    print("Scanning:")
+                    
+                    scanlist = self.wlan.scan()
+                    print("Got Scanlist")
+                    for result in scanlist:
+                      ssid, bssid, channel, RSSI, authmode, hidden = result
+                      print("     %s == %s,  authmode=%d" % (ssid, ubinascii.hexlify(bssid), authmode))
 
-                print("Scan Complete")
-                
-                
-                self.wlan.connect(self.m_ssid, self.m_password)
-                
-                while True:
-                    print('waiting for connection...  Status=%d, connected=%d' % (self.wlan.status(), self.wlan.isconnected()))
-                    if self.wlan.status() < 0 or self.wlan.status() >= 3:
-                        break
-                    time.sleep(1)
+                    print("Scan Complete")
+                    
+                    
+                    self.wlan.connect(self.m_ssid, self.m_password)
+                    
+                    while True:
+                        print('waiting for connection...  Status=%d, connected=%d' % (self.wlan.status(), self.wlan.isconnected()))
+                        if self.wlan.status() < 0 or self.wlan.status() >= 3:
+                            break
+                        time.sleep(1)
 
-                if self.wlan.isconnected():
-                    print('connected')
-                    status = self.wlan.ifconfig()
-                    print( 'ip = ' + status[0] )
-                else:
-                    raise RuntimeError('network connection failed')
+                    if self.wlan.isconnected():
+                        print('connected')
+                        status = self.wlan.ifconfig()
+                        print( 'ip = ' + status[0] )
+                    else:
+                        raise RuntimeError('network connection failed')
 
-                addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+                    addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
 
-                s = socket.socket()
-                s.bind(addr)
-                s.listen(1)
+                    s = socket.socket()
+                    s.bind(addr)
+                    s.listen(1)
 
-                print('listening on', addr)
+                    print('listening on', addr)
 
-                # Listen for connections
-                while True:
-                    try:
-                        cl, addr = s.accept()
-                        print('client connected from', addr)
-                        
-                        cl.send("Welcome to OpenCoffee")
-                        
-                        self.m_lock.acquire()
-                        self.m_socket = cl
-                        self.m_connected = True
-                        connected = True
-                        self.m_lock.release()
-                        
-                        # Wait for disconnect
-                        while connected:
-                            sleep(1)
+                    # Listen for connections
+                    needExit = False
+                    while needExit == False:
+                            cl, addr = s.accept()
+                            print('client connected from', addr)
+                            
+                            cl.send("Welcome to OpenCoffee")
+                            
                             self.m_lock.acquire()
-                            connected = self.m_connected
+                            self.m_socket = cl
+                            self.m_connected = True
+                            connected = True
                             self.m_lock.release()
                             
-                        print("Going back for a relisten")
+                            # Wait for disconnect
+                            while connected:
+                                sleep(1)
+                                self.m_lock.acquire()
+                                connected = self.m_connected
+                                self.m_lock.release()
+                                
+                            print("Going back for a relisten")
+                            needExit = False;
+                            
+                            self.m_lock.acquire()
+                            needExit = self.m_Exit
+                            self.m_lock.release()
 
-                    except OSError as e:
-                        cl.close()
-                        print('connection closed')
+                     
+                    print("Closing Listening Socket")
+                    s.close()
+                    sleep(5)
                 
-                
-                
-                sleep(5)
+        except KeyboardInterrupt as e:
+                self.close()
     
     def __init__(self):
         self.wlan = network.WLAN(network.STA_IF)
@@ -98,7 +112,7 @@ class WifiLog(object):
         if self.m_connected:
             print("C: Log %s" % (data))
             try:
-                self.m_socket.write("Log %s" % (data))
+                self.m_socket.write("Log %s\r\n" % (data))
             except OSError as e:
                 self.m_socket.close()
                 print('connection closed')
@@ -115,83 +129,37 @@ w = WifiLog()
 #w.log("hi")
 #w.connect_thread()
 
+
+
 def xyz():
-    w.connect_thread()
-    print("Out")
+    try:
+        w.connect_thread()
+        print("Wifi Gone")
+    except OSError as e:
+        w.close()
+        print('Closing Up...')
+        
+    except KeyboardInterrupt as e:
+        w.close()
+        print('Keyboard-Closing Up...')
     
 def mno():
     global w
-    i = 0
-    while True:        
-        w.log("Boo: %d" % (i))
-        i = i + 1
-        sleep(2)
-        
+    sleep(5)
+    w.close()
+    
+    #i = 0
+    #while True:        
+    #    w.log("Boo: %d" % (i))
+    #    i = i + 1
+    #    sleep(2)
+   
+try:
+    _thread.start_new_thread(mno, ())
+    xyz()
 
-_thread.start_new_thread(mno, ())
-
-xyz()
-#xyz()
-
-i=0
-
-exit(1)
-
-my_ssid = 'Hello'
-password = 'deadbeef01'
-
-
-
-
-
-
-
-#https://docs.micropython.org/en/v1.8/esp8266/library/network.html?highlight=wlan#network.WLAN
-
-
-print("Done Scan, ssid=%s  pwd=%s" % (my_ssid, password))
-
-wlan.connect(my_ssid, password)
-
-
-max_wait = 10
-while max_wait > 0:
-    print('waiting for connection...  Status=%d, connected=%d' % (wlan.status(), wlan.isconnected()))
-    if wlan.status() < 0 or wlan.status() >= 3:
-        break
-    max_wait -= 1
-    time.sleep(1)
-
-if wlan.isconnected():
-    print('connected')
-    status = wlan.ifconfig()
-    print( 'ip = ' + status[0] )
-else:
-    raise RuntimeError('network connection failed')
-
-addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
-
-s = socket.socket()
-s.bind(addr)
-s.listen(1)
-
-print('listening on', addr)
-
-# Listen for connections
-while True:
-    try:
-        cl, addr = s.accept()
-        print('client connected from', addr)
-        #request = cl.recv(1024)
-        #print(request)
-        #request = str(request)
-        
-        for i in range(1, 99999999999):
-            ret = cl.send("%d, %d\r\n" % (i, i))
-            print("RET: %d" % (ret))
-            
-        cl.close()
-
-    except OSError as e:
-        cl.close()
-        print('connection closed')
+except KeyboardInterrupt as e:
+    w.close()
+    
+    
+print("Bye!!")
