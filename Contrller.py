87ca486e5d5784b909865096bf6fb0e8fae68922
Chris Gray <chgray@gmail.com>
2023-02-06 23:10:29 -0800

    cleaning up nice

--- Contrller.py
+++ Contrller.py
@@ -10,13 +10,18 @@ from time import sleep
 from machine import Pin, PWM
 from time import sleep
 
+from Rotary import Rotary
+import time
+from RotaryIRQ import RotaryIRQ
+
+
 global lcd
 # https://microcontrollerslab.com/ds18b20-raspberry-pi-pico-micropython-tutorial/
 
 
 #https://github.com/miketeachman/micropython-rotary
 
-print("Running.")
+print("123_Running.")
 
 #led = Pin(25, Pin.OUT)
 heater = Pin(26, Pin.OUT)
@@ -87,9 +92,6 @@ lcd.message('Temperature (C)')
 lcd.clear()
 
 
-from Rotary import Rotary
-import time
-from RotaryIRQ import RotaryIRQ
 
 rotary = RotaryIRQ(pin_num_clk=2, 
               pin_num_dt=1, 
@@ -119,134 +121,219 @@ f.write("Time, Time, Temp, Control, Control, P, I, D\r\n")
 
 # 0.2, 0, 2 is pretty good (slow ramp, but holds well)
 
-p = 0.06 #pX / 100
+p = 0.3 #pX / 100
 i = 0
 # http://brettbeauregard.com/blog/2011/04/improving-the-beginner%e2%80%99s-pid-sample-time/
-goalTemp = 105
+goalTemp = 95.3
 #i = 0.1 # most likely half this
-d = 3
+d = 2
+
+with open('PID.config') as configFile:
+    value = configFile.readlines()
+    print("Value: [%s] - %s" % (value, value[0]))
+    bits = value[0].split(",")
+    print(bits[0])
+    print(bits[1])
+    print(bits[2])
+    p = (float)(bits[0]) / 10
+    i = (float)(bits[1]) / 10
+    d = (float)(bits[2]) / 10
+
 
 pid = PID(p,i, d, setpoint=goalTemp, scale='ms')
 pid.output_limits = (0, 1)    # Output value will be between 0 and 10
 pid.set_auto_mode(True, last_output=0)
     
-heater_time = t0
-heaterOn = False
+
 heater.value(0)
 rotary.set(goalTemp)
 count = 0
 t0 = time.ticks_ms()
+
+control = 0.0
+heater_time = t0
 switch_time = t0
+heater_cycle_time = t0
+heater_off_time = t0
 
 
 def toggle_heater(timer):
-    global switch_time
     global heater
-    delta = switch_time - time.ticks_ms()
+    global heater_off_time
+    global heater_cycle_time
+         
+    # Decide if we need to turn off the heater due to our % time being up
+    if heater_off_time != -1 and time.ticks_ms() > heater_off_time: 
+        delta = time.ticks_ms() 
+        #print(" ON_Delta %d, %f" % (heater_off_time - delta, control))
+        heater_off_time = -1
     
-    if delta > 0:
-        print("Delta %d, %d" % (delta, heaterOn))
-        #if heaterOn:
-        #    heater.value(1)
-        #else:
-        #    heater.value(0)
+    # Decide if we're beyond the cycle time period 
+    if time.ticks_ms() > heater_cycle_time: 
+        delta = time.ticks_ms() 
+        #print("OFF_Delta %d, %f" % (heater_cycle_time - delta, control))
+        heater_cycle_time = -1
+    
+    # Recompute stats if we're there
+    if heater_cycle_time == -1:
+        duration = control * 5000      
+                                
+        #if duration < 50 and duration != 0:
+        #    duration = 50
+        sleepTime = 5000 - duration   
+        
+        heater_off_time = time.ticks_ms() + duration
+        heater_cycle_time = time.ticks_ms() + (duration + sleepTime)
+        
+        # Turn On Heater
+        if duration != 0:
+            print("ON : %d, %d" % (duration, sleepTime))
+            heater.value(1)
             
+    if -1 == heater_off_time:
+        #if heater.value() == 1:
+        #    print("OFF")
+        heater.value(0)
             
-
-timer.init(freq=10, mode=Timer.PERIODIC, callback=toggle_heater)
+timer.init(freq=100, mode=Timer.PERIODIC, callback=toggle_heater)
 
         
 try:
     nextPrintTime = 0
-    tOld = time.ticks_ms()
+    t_start = time.ticks_ms()
+    mode = 0
     while True :        
-        f.flush()        
+        f.flush()  
+        t = time.ticks_ms()      
         try:
-            b = button.value()            
-            
+            b = button.value()  
             display=False
-            lcdDisplay = False
+            lcdDisplay = False 
+            updatePID = False   
+            updateConfig = False
             
-            if goalTemp != rotary.value():
-                goalTemp = rotary.value()
-                print("New Set Point : %d" % goalTemp)                
-                lcdDisplay = True
-                
-            pid.setpoint = goalTemp
-            
-            #sleep(5)
-            #print("Delta %d" % (time.ticks_ms() - tOld))
-            tOld = time.ticks_ms() 
-              
-            #sleep(0.05)
+            if mode == 0:
+                if goalTemp != rotary.value():
+                    goalTemp = rotary.value()
+                    print("New Set Point : %d" % goalTemp)                
+                    lcdDisplay = True 
+                    updatePID = True
+                    updateConfig = True
+                    pid.set_auto_mode(True)
+            elif mode == 1:
+                if p != rotary.value()/10:
+                    p = rotary.value()/10
+                    print("New P : %f" % p)                
+                    lcdDisplay = True
+                    updatePID = True
+                    updateConfig = True
+            elif mode == 2:
+                if i != rotary.value()/10:
+                    i = rotary.value()/10
+                    print("New I : %d" % i)                
+                    lcdDisplay = True
+                    updatePID = True
+                    updateConfig = True
+            elif mode == 3:
+                if d != rotary.value()/10:
+                    d = rotary.value()/10
+                    print("New D : %d" % d)                
+                    lcdDisplay = True
+                    updatePID = True
+                    updateConfig = True
+            elif mode == 4:
+                if control != rotary.value()/100:
+                    control = rotary.value()/100
+                    print("Manual Control : %f, %d" % (control, rotary.value()))               
+                    lcdDisplay = True
+                    updatePID = True
+                    updateConfig = True   
+                    pid.set_auto_mode(False)                 
+                                        
             if time.ticks_ms() > switch_time:                
-                temp = max.read()
-                t = time.ticks_ms() - t0 # t is CPU seconds elapsed (floating point)
-                t_start = time.ticks_ms() - t_init            
-            
-                print("Delta : %d (%d)" % (time.ticks_ms() - switch_time, heaterOn))
-            
+                temp = max.read()            
+                pid.setpoint = goalTemp    
                 # Compute new output from the PID according to the systems current value
-                control = pid(temp)            
-                duration = control * 2000      
-                                
-                if duration < 250 and duration != 0:
-                    duration = 250
-                sleepTime = 2000 - duration                                                   
                 
-                if heaterOn:
-                    switch_time = time.ticks_ms() + (sleepTime)
-                    if sleepTime != 0:
-                        heater.value(0)                                                 
-                    heaterOn = False
-                else:                    
-                    switch_time = time.ticks_ms() + (duration)
-                    if duration != 0:
-                        heater.value(1)
-                       
-                    heaterOn = True
-                    display = True
-                   
+                if mode != 4:
+                    control = pid(temp) 
+                else:
+                    control = rotary.value()
+                    
+                switch_time = time.ticks_ms() + 2000
+                #print("Recalculated.")                   
                               
             if (time.ticks_ms() > nextPrintTime) or display:
                 lcdDisplay = True
                 nextPrintTime = time.ticks_ms() + 2000
-                #print("Duration %d, sleepUntil %d, sleepTime %d, heaterOn %d" % (duration, switch_time, sleepTime, heaterOn))    
-                print("Val, %d, %d, %f, %f, %d, %f, %f, %f" % (t_start, t, temp, control, control, p, i, d))          
-                #f.write("%d, %d, %f, %f, %d, %f, %f, %f\r\n" % (t_start, t, temp, control, control, p, i, d))
+                print("Val, %d, %d, %d, %f, %f, %f, %f, %f    " % (t - t_start, t, goalTemp, temp, control, p, i, d))          
+                f.write("%d, %d, %d, %f, %f, %f, %f, %f\r\n" % (t - t_start, t, goalTemp, temp, control, p, i, d))
                 
-            if lcdDisplay:
-                lcd.clear()
-                lcd.write(0, 0, ('Goal: %d(C), %f' % (goalTemp,control)))
-                lcd.write(0, 1, ("%f" % temp))
-                #print("Next Switch %d" % (switch_time - time.ticks_ms()))
-                #switch_time = time.ticks_ms() + 100000
                 
-            #heater.value(1)
-            #sleep(duration / 1000)
-            #if sleepTime != 0:
-            #    heater.value(0)
-            #    sleep(sleepTime / 1000)
-                
-            
-            count = count + 1
-            
-            if 0 == button.value():
-                goalTemp += 5
-                print("GoalTemp: %d" % goalTemp)
+            if updatePID:
                 pid = PID(p, i, d, setpoint=goalTemp, scale='ms')
                 pid.output_limits = (0, 1)    # Output value will be between 0 and 10
                 pid.set_auto_mode(True, last_output=0)
-                sleep(.25)
+                print("PID UPDATED")
+                
+            if 0 == button.value():
+                mode = mode + 1
+                if mode > 4:
+                    mode = 0              
+                lcdDisplay = True
+                            
+                if 0 == mode:                 
+                    rotary = RotaryIRQ(pin_num_clk=2, pin_num_dt=1,  min_val=0,max_val=110, reverse=False, range_mode=RotaryIRQ.RANGE_BOUNDED)
+                    rotary.set(goalTemp)
+                if 1 == mode:
+                    rotary = RotaryIRQ(pin_num_clk=2, pin_num_dt=1,  min_val=0,max_val=40, reverse=False, range_mode=RotaryIRQ.RANGE_BOUNDED)
+                    rotary.set(p*10)
+                if 2 == mode:
+                    rotary = RotaryIRQ(pin_num_clk=2, pin_num_dt=1,  min_val=0,max_val=40, reverse=False, range_mode=RotaryIRQ.RANGE_BOUNDED)
+                    rotary.set(i * 10)
+                if 3 == mode:
+                    rotary = RotaryIRQ(pin_num_clk=2, pin_num_dt=1,  min_val=0,max_val=40, reverse=False, range_mode=RotaryIRQ.RANGE_BOUNDED)
+                    rotary.set(d * 10)
+                if 4 == mode:
+                    rotary = RotaryIRQ(pin_num_clk=2, pin_num_dt=1,  min_val=0,max_val=100, reverse=False, range_mode=RotaryIRQ.RANGE_BOUNDED)
+                    rotary.set((int)(control*100))
+                
+                sleep(.25)                
+           
+           
+            if lcdDisplay:
+                lcd.clear()
+                if 0 == mode:
+                    lcd.write(0, 0, ('Goal: %d(C), %f' % (goalTemp,control)))
+                    lcd.write(0, 1, ("%f" % temp))    
+                if 1 == mode:
+                    lcd.write(0, 0, ("P = %f" % (p)))
+                if 2 == mode:
+                    lcd.write(0, 0, ("I = %f" % (i)))
+                if 3 == mode:
+                    lcd.write(0, 0, ("D = %f" % (d)))
+                if 4 == mode:
+                    lcd.write(0, 0, "*MANUAL HEATER*")
+                    lcd.write(0, 1, ("%f %f(C)" % (control, temp)))
+                    
+                    
+            if updateConfig:
+                with open("PID.config", 'w') as save:
+                    save.write("%d,%d,%d" % (p*10, i*10, d*10))
+            count = count + 1
+            
+
                 
         except OSError:
             heater.value(0)
+            timer.deinit()
             #f.write("Crashed")            
             print("OSError")
             
 
 except KeyboardInterrupt:
     heater.value(0)
+    timer.deinit()
     f.write("Crashed")
     print(e)
     print("Debugger Stopped..")
